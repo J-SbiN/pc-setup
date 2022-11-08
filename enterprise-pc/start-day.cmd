1fd02785866d36488ecaf517d76fdda3b4574c08
jsabino@pt02-2000wlxhni <jorgemiguelsabino@gmail.com>
2022-11-08 15:37:57 +0000

    added file

---
+++ enterprise-pc/start-day.cmd
@@ -0,0 +1,8 @@
+"C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
+
+code "\\wsl$\Ubuntu\home\jsabino\gls\logbook" | exit 0
+
+:: "C:\Program Files\cmder\vendor\conemu-maximus5\ConEmu.exe" | exit 0
+"C:\Program Files\cmder\Cmder.exe"
+
+
